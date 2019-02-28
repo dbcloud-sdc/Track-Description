87a55f8872338ab91e6f867c3f6d227f1dd30f45
Jacky <jackyleichicago@gmail.com>
2019-02-28 11:10:05 -0800

    Implement stubbed out crud routes

---
+++ server/routers/songInfoRoute.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/:song_id', (req, res) => {
+
+})
+
+router.post('/', (req, res) => {
+
+})
+
+router.put('/:song_id', (req, res) => {
+
+})
+
+router.delete('/:song_id', (req, res) => {
+
+})
+
+module.exports = router;
