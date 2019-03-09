f6e5b531ad01094b390e16c5c83746396d880507
Jacky <jackyleichicago@gmail.com>
2019-03-09 12:18:38 -0800

    Remove un-used middleware from server to improve request throughput speed

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
+require('newrelic');
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 const { read } = require('./controllers/index');
@@ -8,14 +8,10 @@ const app = express();
 
 const port = 8081;
 
-app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/api/song/:song_id', read);
-// app.use('/:song_id', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../public/index.html'));
-// });
 
 app.use('/:sid', express.static(path.join(__dirname, '../public')));
 
