93ce21a4ca8d64d98399c0313d7f2bea135d672f
Jacky <jackyleichicago@gmail.com>
2019-03-08 21:01:14 -0800

    Implement server models and controllers
    Sets up models and controllers to satisfy client requests to preset
    routes.

---
+++ server/controllers/index.js
@@ -0,0 +1,38 @@
+const models = require ('../models/index.js');
+
+const controllers = {
+  read: (req, res) => {
+    models.fetchSongInfo(req.params.song_id, (err, data) => {
+      if (err) {
+        res.status(201).send(err);
+      } else {
+        const results = data.rows[0];
+        let trackInfo = {
+          song: {
+            id: Number(req.params.song_id),
+            song_name: results.song_name,
+            artist_id: results.artist_id,
+            license: results.license,
+            description_text: results.description_text,
+            released_by: results.released_by,
+            release_date: results.release_date,
+            p_line: results.p_line,
+            tags: results.tags
+          },
+          artist: {
+            id: results.id,
+            artist_name: results.artist_name,
+            avatar_picture: results.avatar_picture,
+            no_of_followers: results.no_of_followers,
+            no_of_tracks: results.no_of_tracks,
+            pro_status: results.pro_status,
+            is_followed: results.is_followed
+          }
+        };
+        res.status(200).send(trackInfo);
+      }
+    })
+  }
+}
+
+module.exports = controllers; 
--- server/index.js
+++ server/index.js
@@ -2,10 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
-const artistInfoRoute = require('./routers/artistInfoRoute.js');
-const songInfoRoute = require('./routers/songInfoRoute.js');
-
-const db = require('./db');
+const { read } = require('./controllers/index');
 
 const app = express();
 
@@ -15,8 +12,9 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.use('/api/artistinfo', artistInfoRoute);
-app.use('/api/songinfo', songInfoRoute);
-app.use('*', express.static(path.join(__dirname, '../client/dist')));
+app.get('/api/song/:song_id', read);
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 app.listen(port, () => console.log(`LISTENING ON PORT ${port}`));
